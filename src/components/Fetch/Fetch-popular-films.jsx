5b5bd95960046aa495bf2e2bc1c6f0a683264d07
KizimJule <kizimjule@gmail.com>
2022-11-14 20:24:23 +0100

    fetchPopular

--- src/components/Fetch/Fetch-popular-films.jsx
+++ src/components/Fetch/Fetch-popular-films.jsx
@@ -1,10 +1,18 @@
 import axios from 'axios';
+import PropTypes from 'prop-types';
 
 import { KEY, BASE_URL } from './Key-url';
 
-export async function PopFilmsAPI(page = 1) {
+export async function getPopFilms(page = 1) {
   const url = `${BASE_URL}/trending/movie/day?api_key=${KEY}&page=${page}`;
   const { data } = await axios.get(url);
+  //   console.log(data.results);
+  //   console.log(data, 'data');
 
-  return data;
+  return data.results;
 }
+getPopFilms();
+
+getPopFilms.propTypes = {
+  page: PropTypes.number,
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,29 @@
 import { Container } from 'components/SharedLayout/SharedLayout.styled';
+import { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { getPopFilms } from '../components/Fetch/Fetch-popular-films';
 
 export const Home = () => {
+  const [popFilms, setPopFilms] = useState([]);
+  const location = useLocation();
+
+  useEffect(() => {
+    getPopFilms().then(setPopFilms);
+  }, []);
+
   return (
     <main>
       <Container>
+        <ul>
+          {popFilms.map(popFilm => (
+            <li key={popFilm.id}>
+              <Link to={`/movies/${popFilm.id}`} state={{ from: location }}>
+                {popFilm.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+
         <h1>Tranding today</h1>
         <ul>
           <li>Film 1</li>
